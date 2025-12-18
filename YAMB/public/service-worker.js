7fc0269131f46214052277f94757342277a9c7cd
Gligor Isaev <isaev.g@loh-services.de>
2025-12-18 09:09:26 +0100

    Add PWA support for installable app on iPhone and Android

---
+++ YAMB/public/service-worker.js
@@ -0,0 +1,41 @@
+// Service Worker for Yamb PWA
+const CACHE_NAME = 'yamb-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/static/js/main.js',
+        '/static/css/main.css'
+      ]).catch(() => {
+        // Ignore cache errors during install
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
